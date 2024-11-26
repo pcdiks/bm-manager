05cf8d3a14ebadec749220f5472b49f6089c83cc
Paul-Christiaan Diks <16719277+pcdiks@users.noreply.github.com>
2024-11-26 11:47:16 +0100

    Update .gitignore and modify ALLOWED_HOSTS in settings.py for improved configuration

--- bookmark_manager/settings.py
+++ bookmark_manager/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # General settings
 SECRET_KEY = config('SECRET_KEY', default='dummy-secret-key-for-build')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 
 # Internationalization
 LANGUAGE_CODE = config('LANGUAGE_CODE', default='en-us')
