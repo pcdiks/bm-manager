ccc77429df3d0921c0ac5dfe0865814512330757
Paul-Christiaan Diks <pc@diks.org>
2025-06-05 15:01:08 +0200

    Fix unauthorized collection access in home view

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Collection
+
+
+class HomeViewTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="user1", password="pass")
+        self.user2 = User.objects.create_user(username="user2", password="pass")
+        self.collection1 = Collection.objects.create(
+            name="c1", owner=self.user1
+        )
+
+    def test_user_cannot_access_others_collection(self):
+        """Home view should return 404 for a collection not owned by the user."""
+        self.client.login(username="user2", password="pass")
+        response = self.client.get(
+            reverse("home"), {"collection": self.collection1.id}
+        )
+        self.assertEqual(response.status_code, 404)
+
